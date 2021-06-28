09f2d7e2c0661cfffa0b4afd4d032024d9a758ab
Rudá Torquato <rudatorquato@gmail.com>
2021-06-28 16:45:50 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/shared/models/transaction_model.dart';
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:intl/intl.dart';
 
 void main() {
