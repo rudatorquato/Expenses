b342ba3f10d2f8419298f0be8f6617aced3f69f8
Rudá Torquato <rudatorquato@gmail.com>
2021-07-01 11:27:57 -0300

    refatoração

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,9 +1,5 @@
-import 'package:expenses/core/app_text_styles.dart';
-import 'package:expenses/shared/models/transaction_model.dart';
-import 'package:expenses/transaction/widgets/form/transaction_form_widget.dart';
+import 'package:expenses/home/home_page.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'package:expenses/transaction/widgets/list/transaction_list_widget.dart';
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({Key? key}) : super(key: key);
@@ -17,94 +13,6 @@ class ExpensesApp extends StatelessWidget {
         primaryColor: Colors.purple[600],
         accentColor: Colors.purple[100],
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  //const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _transactions = [
-    // Transaction(
-    //   id: 't1',
-    //   title: 'Novo tenis de corrida',
-    //   value: 310.10,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: 'orrida',
-    //   value: 210.10,
-    //   date: DateTime.now(),
-    //),
-  ];
-
-  _addTransaction(String title, double value) {
-    final newTransaction = Transaction(
-      id: Random().nextDouble().toString(),
-      title: title,
-      value: value,
-      date: DateTime.now(),
-    );
-
-    setState(() {
-      _transactions.add(newTransaction);
-    });
-
-    Navigator.of(context).pop();
-  }
-
-  _openTransactionFormModal(BuildContext context) {
-    showModalBottomSheet(
-        context: context,
-        builder: (_) {
-          // ignore: avoid_init_to_null
-          //var onSubmit = null;
-          return TransactionForm(_addTransaction);
-        });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Despesas Pessoais',
-          style: AppTextStyles.openSans,
-        ),
-        actions: [
-          IconButton(
-            onPressed: () => _openTransactionFormModal(context),
-            icon: Icon(Icons.add),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('Grafico'),
-                elevation: 5,
-              ),
-            ),
-            TransactionList(transactions: _transactions),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _openTransactionFormModal(context),
-        child: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
