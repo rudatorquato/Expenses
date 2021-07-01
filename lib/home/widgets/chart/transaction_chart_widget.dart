86ba5634371f602656173348895f8bb477037d97
Rudá Torquato <rudatorquato@gmail.com>
2021-07-01 11:40:10 -0300

    Chart widget #01

---
+++ lib/home/widgets/chart/transaction_chart_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:intl/intl.dart';
+import 'package:expenses/shared/models/transaction_model.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+
+  const Chart({
+    Key? key,
+    required this.recentTransaction,
+  }) : super(key: key);
+
+  List<Map> get groupedTransaction {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double vSum = 0.0;
+
+      for (var i = 0; i < recentTransaction.length; i++) {
+        bool sameDay = recentTransaction[i].date.day == weekDay.day;
+        bool sameMonth = recentTransaction[i].date.month == weekDay.month;
+        bool sameYear = recentTransaction[i].date.year == weekDay.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          vSum += recentTransaction[i].value;
+        }
+      }
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': vSum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
