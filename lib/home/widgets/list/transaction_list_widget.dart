2b4c42934ca796297f7738e6c6ae8b4ab4969790
Rudá Torquato <rudatorquato@gmail.com>
2021-07-05 11:19:57 -0300

    usando listTile

--- lib/home/widgets/list/transaction_list_widget.dart
+++ lib/home/widgets/list/transaction_list_widget.dart
@@ -41,46 +41,32 @@ class TransactionList extends StatelessWidget {
               itemBuilder: (ctx, index) {
                 final e = transactions[index];
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'R\$  ${e.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColor,
-                          ),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('R\$${e.value}'),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            e.title,
-                            style: AppTextStyles.openSansNameProduct,
-                          ),
-                          Text(
-                            DateFormat('d MMM y').format(e.date),
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      e.title,
+                      style: AppTextStyles.openSansNameProduct,
+                    ),
+                    subtitle: Text(
+                      DateFormat('d MMM y').format(e.date),
+                    ),
                   ),
                 );
-              }),
+              },
+            ),
     );
   }
 }
