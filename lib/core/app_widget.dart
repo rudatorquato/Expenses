c988c747b6fdce33aea70a2004511a0f7f4f00fc
Rudá Torquato <rudatorquato@gmail.com>
2021-11-24 16:42:04 -0300

    banner debug

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -8,6 +8,7 @@ class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
       theme: ThemeData(
         //primarySwatch: Colors.purple,
